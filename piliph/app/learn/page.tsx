ea2995fdc69669909ca966694f98b5b7ea744668
Mikhan-TMP <mikhanbalbastro@outlook.com>
2026-09-06 00:24:05 +0800

    Added about page and removed the bg from learn page

--- piliph/app/learn/page.tsx
+++ piliph/app/learn/page.tsx
@@ -92,7 +92,7 @@ const filteredArticles = articles.filter((art) => {
 });
 
 return (
-    <main className="flex-1 overflow-y-auto p-6 md:p-8 bg-[#F8F9FA] min-h-screen text-slate-800 font-sans">
+    <main className="flex-1 overflow-y-auto p-6 md:p-8  min-h-screen text-slate-800 font-sans">
     {/* Top Banner */}
     <div className="flex flex-col lg:flex-row lg:items-center justify-between gap-6 mb-8 border-b border-slate-200 pb-6">
         <div>
